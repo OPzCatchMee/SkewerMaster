6fc6dbf3420beb18d704aa08993ef93ebe99b287
OPzCatchMee <cmnguyen999@gmail.com>
2017-11-09 11:37:47 -0600

    Added start menu/button

---
+++ Skewer Master/Assets/scripts/LoadGame.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class LoadGame : MonoBehaviour {
+
+	public Button yourButton;
+
+	void Start()
+	{
+		Button btn = yourButton.GetComponent<Button>();
+		btn.onClick.AddListener(TaskOnClick);
+	}
+
+	// Click the start button
+	void TaskOnClick() {
+		Scene scene = SceneManager.GetActiveScene ();
+		SceneManager.LoadScene ("Level1");
+	}
+}
