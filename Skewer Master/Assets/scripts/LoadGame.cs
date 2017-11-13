dcdad3520493f1c0bc57b1a5ffd4fdcb38d29d42
OPzCatchMee <cmnguyen999@gmail.com>
2017-11-12 23:54:07 -0600

    Added homepage with start/help button

--- Skewer Master/Assets/scripts/LoadGame.cs
+++ Skewer Master/Assets/scripts/LoadGame.cs
@@ -1,22 +1,10 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine.UI;
-using UnityEngine.SceneManagement;
 using UnityEngine;
 
 public class LoadGame : MonoBehaviour {
 
-	public Button yourButton;
-
-	void Start()
-	{
-		Button btn = yourButton.GetComponent<Button>();
-		btn.onClick.AddListener(TaskOnClick);
-	}
-
 	// Click the start button
-	void TaskOnClick() {
-		Scene scene = SceneManager.GetActiveScene ();
-		SceneManager.LoadScene ("Level1");
+	public void startGame(string level) {
+		Application.LoadLevel (level);
 	}
 }
